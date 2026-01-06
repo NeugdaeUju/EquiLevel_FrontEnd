96e266d913f20586aab0b5524fc87289d9aee10e
Leïla Pelletier <diane.arghat@gmail.com>
2026-01-06 16:01:30 +0100

    Création de la logique de déconnexion

--- src/components/HeadbandEdit.tsx
+++ src/components/HeadbandEdit.tsx
@@ -1,13 +1,22 @@
 import { FaPenToSquare } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../features/auth/authSlice';
 import '../assets/scss/headbandEdit.scss';
 
 function HeadBandEdit() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate('/');
+    };
     return (
         <div className='headband'>
             <FaPenToSquare className='headband__icon'/>
             <p className='headband__text'>Edit Mode</p>
-            <Link to='/' className='headband__link'>LogOut</Link>
+            <Link to='/' className='headband__link' onClick={handleLogout}>LogOut</Link>
         </div>
     )
 }
