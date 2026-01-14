baa7a4bb52c11349849a85a2c1b5581329f13112
Leïla Pelletier <diane.arghat@gmail.com>
2026-01-14 11:53:40 +0100

    Mise à jour de l'affichage des pages de races avec ajout de la modal pour créer une nouvelle race

--- src/pages/SpecialsHorses.tsx
+++ src/pages/SpecialsHorses.tsx
@@ -1,10 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import Modal from '../components/Modal.tsx';
+import RacesForm from '../components/RacesForm.tsx';
+import type { RootState, AppDispatch } from '../store/store.ts'
+import { FaPenToSquare } from "react-icons/fa6";
+import { createEspece } from '../features/especes/especesSlice.ts';
 import { Link } from 'react-router-dom'
-import '../assets/scss/donkeys.scss'
+import '../assets/scss/races.scss'
 
 function SpecialsHorses() {
+    const token = useSelector((state: RootState) => state.auth.token)
+    //const especes = useSelector((state: RootState) => state.especesGet.especes)
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const dispatch = useDispatch<AppDispatch>()
+    const [name, setName] = useState('');
+
+    /*useEffect(() => {
+        dispatch(fetchEspeces());
+    }, [dispatch]);*/
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        dispatch(createEspece(name))
+        console.log('Vous avez créer une especes :', name)
+    }
+
     return (
         <div className='container'>
-            <h1 className='container__title' >Chevaux divins et spéciaux</h1>
+            <div className='container__hero'>
+                <h1 className='container__hero__title' >Chevaux divins et spéciaux</h1>
+                { token && 
+                    <button onClick={() => setIsModalOpen(true)} className='races-container__hero--button'> <FaPenToSquare/> Ajouter une race </button>}
+                    <Modal
+                        isOpen={isModalOpen}
+                        onClose={() => setIsModalOpen(false)}
+                        title='Ajouter une race'>
+                            <RacesForm
+                                handleSubmit={handleSubmit}
+                                setRacesName={setName}
+                                racesName={name}/>
+                    </Modal>
+            </div>
             <div className='container__links'>
                 <Link to='/all-races' className='container__links--link'>Toutes les races</Link>
                 <Link to='/especes' className='container__links--link'>Voir toutes les expèces</Link>
