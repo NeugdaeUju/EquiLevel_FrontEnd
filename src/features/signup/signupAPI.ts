f476ca431b8cb6312e30ff36fac21ab54bdb4f16
Leïla Pelletier <leila.plltr@gmail.com>
2026-06-03 16:03:56 +0200

    Nettoyage du code

--- src/features/signup/signupAPI.ts
+++
@@ -1,6 +0,0 @@
-import apiRequest from "../../api/api.ts";
-
-export const signupUser = async (email: string, password: string) => {
-    const response = await apiRequest.post('/auth/signup', { email, password });
-    return response.data;
-};
--- src/pages/Especes.tsx
+++
@@ -1,9 +0,0 @@
-function Especes() {
-    return(
-        <div>
-            <h1>Espèces</h1>
-        </div>
-    )
-}
-
-export default Especes;
