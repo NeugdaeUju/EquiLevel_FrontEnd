c246d71869bbff828e53734570bddef407a60460
Leïla Pelletier <leila.plltr@gmail.com>
2026-08-28 14:45:34 +0200

    Ajout des cards de résumé (valeurs fixes)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ import '../assets/styles/header.css';
 function Header() {
 
     return (
-        <div class='header'>
+        <div className='header'>
             <h1>Tableau de bord</h1>
-            <div class='header-actions'>
-                <input type='search' placeholder='Recherche...' class='research'></input>
-                <button type='button' class='add-horse'>Nouveau Cheval</button>
+            <div className='header-actions'>
+                <input type='search' placeholder='Recherche...' className='research'></input>
+                <button type='button' className='add-horse'>Nouveau Cheval</button>
             </div>
         </div>
     )
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import Cards from '../components/Cards';
 import '../assets/styles/dashboard.css';
 
 function Dashboard() {
@@ -8,6 +9,24 @@ function Dashboard() {
       <Sidebar />
       <main className="dashboard-content">
         <Header />
+        <div className='cards-container'>
+          <Cards 
+            title="Total chevaux"
+            number={12}
+            description="3 poulains · 9 adultes"/>
+            <Cards 
+            title="BLUP 100 ATTEINT"
+            number={4}
+            description="↑ +1 ce mois"/>
+            <Cards 
+            title="en progression"
+            number={6}
+            description="Entraînement actif"/>
+            <Cards 
+            title="BLUP moyen"
+            number={84}
+            description="Objectif : 100"/>
+        </div>
       </main>
     </div>
   );
